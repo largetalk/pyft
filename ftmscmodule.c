5df50e23c12ba41599037157efb9a9d3c9e7b18c
Arthur <largetalk@largetalk.localdomain>
2012-03-21 22:15:10 +0800

    add source file

---
+++ ftmscmodule.c
@@ -0,0 +1,36 @@
+#include <Python.h>
+#include <stdio.h>
+#include <string.h>
+#include "qtts.h"
+#include "qisr.h"
+
+static PyObject *FtmscError;
+
+static PyObject* pyQISRInit(PyObject *self, PyObject *args)
+{
+    const char *init_str;
+    int ret;
+
+    if (!PyArg_ParseTuple(args, "s", &init_str))
+        return NULL;
+	ret = QISRInit(init_str);
+    return Py_BuildValue("i", ret);
+}
+
+
+static PyMethodDef FtmscMethods[] = {  
+    {"qinit", pyQISRInit, METH_VARARGS, "exec QISRInit"},  
+    {NULL, NULL, 0, NULL}  
+};  
+
+PyMODINIT_FUNC initftmsc(void)
+{
+    PyObject *m;
+    m = Py_InitModule("ftmsc", FtmscMethods);
+    if (m == NULL)
+        return;
+    FtmscError = PyErr_NewException("ftmsc error", NULL, NULL);
+    Py_INCREF(FtmscError);
+    PyModule_AddObject(m, "error", FtmscError);
+}
+
